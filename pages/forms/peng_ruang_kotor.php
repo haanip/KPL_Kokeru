f01d6b4cf7c8fbad792215c551c0c86173535bee
haanip <75033750+haanip@users.noreply.github.com>
2021-10-17 13:21:09 +0700

    Update peng_ruang_kotor.php

--- pages/forms/peng_ruang_kotor.php
+++ pages/forms/peng_ruang_kotor.php
@@ -33,21 +33,7 @@
       </li>
     </ul>
 
-    <!-- Right navbar links -->
-    <ul class="navbar-nav ml-auto">
-      <li class="nav-item">
-        <a class="nav-link" data-widget="fullscreen" href="#" role="button">
-          <i class="fas fa-expand-arrows-alt"></i>
-        </a>
-      </li
-      <li class="nav-item">
-        <a class="nav-link" data-widget="control-sidebar" data-slide="true" href="#" role="button">
-          <i class="fas fa-th-large"></i>
-        </a>
-      </li>
-    </ul>
-  </nav>
-  <!-- /.navbar -->
+
 
  <!-- Main Sidebar Container -->
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
