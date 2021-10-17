2b1c5172b4b88794bf64f9a534f8f47ccefc26af
haanip <75033750+haanip@users.noreply.github.com>
2021-10-17 13:15:01 +0700

    Update peng_ruang_bersih.php

--- pages/forms/peng_ruang_bersih.php
+++ pages/forms/peng_ruang_bersih.php
@@ -56,6 +56,14 @@
       <img src="../../dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
       <span class="brand-text font-weight-light">KoKeRu</span>
     </a>
+	  
+ <!-- Main Sidebar Container -->
+  <aside class="main-sidebar sidebar-dark-primary elevation-4">
+    <!-- Brand Logo -->
+    <a href="peng_home.php" class="brand-link">
+      <img src="../../dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+      <span class="brand-text font-weight-light">KoKeRu</span>
+    </a>
 
     <!-- Sidebar -->
     <div class="sidebar">
